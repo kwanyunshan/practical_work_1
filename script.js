3e9144ce125ad9f842a1e359a2f452a62307faab
kwanyunshan <yunshan0503@gmail.com>
2026-06-10 21:49:58 +0800

    First Commit

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  $("#btnSubmit").click(function () {
+    var age = parseInt($("#age").val());
+
+    if (age < 13) {
+      alert("Participant is not eligible to register for the event.");
+    } else if (age >= 13 && age <= 17) {
+      alert("Participant must be accompanied by a parent or guardian.");
+    } else if (age >= 18 && age <= 40) {
+      alert("Participant is eligible to participate in all event activities.");
+    } else if (age >= 41 && age <= 60) {
+      alert(
+        "Participant is eligible to participate but is advised to choose suitable activities.",
+      );
+    } else {
+      alert(
+        "Participant is welcome to join the event but is advised to participate in light activities only.",
+      );
+    }
+  });
+});
